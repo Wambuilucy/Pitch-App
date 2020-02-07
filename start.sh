009f9b9cea997262940e00387531140cad1e4b8a
Wambuilucy <wambuilucie99@gmail.com>
2020-02-07 16:25:01 +0300

    first commit

---
+++ start.sh
@@ -0,0 +1,5 @@
+export MAIL_USERNAME='alicemwihaki99@gmail.com'
+export MAIL_PASSWORD='malofa@1998'
+export SECRET_KEY='secret'
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:alice1234@localhost/alice'
+python3.6 manage.py server
