30c4fe8f318251120304b545777a0d73c08174e8
Wambuilucy <wambuilucie99@gmail.com>
2020-02-17 11:07:14 +0300

    resolving 500 error

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
     # debug = True
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     #  email configurations
     
     MAIL_SERVER = 'smtp.gmail.com'
